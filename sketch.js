90799b376002e7903a61751234661fea08f88cfc
wuhbet <105667092+wuhbet@users.noreply.github.com>
2022-05-16 18:41:32 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,5 @@
+
+console.log("hello");
+console.log("my first project")
+console.log("this is class 10 and thus is my tenth lesson in my learning journey to code. For i have learned things from creating sprites to designing")
+console.log("them i have thouroughly enjoyed my time learning and expect even more to come.")
